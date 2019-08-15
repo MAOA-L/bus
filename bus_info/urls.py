6e6364cfd24d040503c2533265415d7fbf496b50
CyanZoy <1099312914@qq.com>
2019-08-15 23:14:30 +0800

    创建应用

---
+++ bus_info/urls.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+"""
+ @Time    : 19/8/15 22:44
+ @Author  : CyanZoy
+ @File    : urls.py
+ @Software: PyCharm
+ @Describe: 
+ """
+from django.conf.urls import url
+
+urlpatterns = [
+
+]
